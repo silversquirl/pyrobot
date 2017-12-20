d4cf29241248a14a4f37cb56ae5d32c5a2f250ba
Samadi van Koten <samadi@vktec.co.uk>
2017-12-20 15:16:27 +0000

    Reduce render quality
    It was taking 5min/flame on my i5 laptop with 8G RAM. I hate to think what
    would've happened when I put it on a VPS.

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read -r flame; do
     out="${flame%.flam3}.png"
-    in="$flame" out="$out" qs=50 flam3-render
+    in="$flame" out="$out" qs=20 flam3-render
     echo "$out"
 done
